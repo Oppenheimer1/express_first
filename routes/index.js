f5ee83c817ec0e83e87e2c37d3a971c1bbfc7949
LARS BERG <larsgbr@gmail.com>
2016-04-23 22:21:40 -0500

    Third Commit, updated and configured routes correctly

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,15 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET index2 page Green Side */
+router.get('/index2', function(req, res, next) {
+  res.render('index2', { title: 'Express' });
+});
+
+/* GET index2 page Blue Side */
+router.get('/index3', function(req, res, next) {
+  res.render('index3', { title: 'Express' });
+});
+
+
 module.exports = router;
